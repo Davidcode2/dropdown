cb9c699816f31b6f7ce30f90d7e9a61ae6bb6445
Jakob Lingel <jlingel68@gmail.com>
2022-09-23 23:02:30 +0200

    Add hover class

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import css from './css/style.css';
 class DropDown {
   makeDropDownButton() {
     const dropDownButtons = document.querySelectorAll('.dropdown');
-    dropDownButtons.forEach((button) => button.addEventListener('click', this.toggleMenu.bind(this)));
+    dropDownButtons.forEach((button) => {
+      if (Array.from(button.classList).includes('dropdown-hover')) {
+        button.addEventListener('mouseenter', this.toggleMenu.bind(this));
+        button.parentNode.addEventListener(
+          'mouseleave',
+          this.toggleMenuOnMouseOut.bind(this),
+        );
+      } else {
+        button.addEventListener('click', this.toggleMenu.bind(this));
+      }
+      console.log(button.classList);
+    });
   }
 
   getAllSiblings(element) {
@@ -16,25 +27,54 @@ class DropDown {
     return siblings;
   }
 
+  getEventTarget(event) {
+    return event.target;
+  }
+
   toggleMenu(event) {
-    event.target.classList.toggle('active');
-    const siblings = this.getAllSiblings(event.target);
-    if (event.target.classList.contains('active')) {
-      for (let i = 1; i <= siblings.length; i++) {
-        setTimeout(() => {
+    const target = this.getEventTarget(event);
+    const targetClassList = Array.from(target.classList);
+    if (targetClassList.includes('dropdown')) {
+      target.classList.toggle('active');
+      const siblings = this.getAllSiblings(target);
+      if (target.classList.contains('active')) {
+        this.animationFoldIn(siblings);
+      } else {
+        this.animationFoldOut(siblings);
+      }
+    }
+  }
+
+  toggleMenuOnMouseOut(event) {
+    const target = this.getEventTarget(event);
+    target.firstElementChild.classList.remove('active');
+    const { children } = target;
+    this.animationFoldOut(children);
+  }
+
+  animationFoldOut(siblings) {
+    for (let i = 1; i <= siblings.length; i++) {
+      setTimeout(() => {
+        if (
+          !Array.from(siblings[siblings.length - i].classList).includes(
+            'dropdown',
+          )
+        ) {
           siblings[siblings.length - i].style.visibility = 'hidden';
           siblings[siblings.length - i].style.opacity = '0';
           siblings[siblings.length - i].style.transition = 'visibility 0.2s linear, opacity 0.2s linear';
-        }, i * 50);
-      }
-    } else {
-      siblings.forEach((elem, index) => {
-        setTimeout(() => {
-          elem.style.visibility = 'visible';
-          elem.style.opacity = '1';
-        }, index * 20);
-      });
+        }
+      }, i * 50);
     }
+  }
+
+  animationFoldIn(siblings) {
+    siblings.forEach((elem, index) => {
+      setTimeout(() => {
+        elem.style.visibility = 'visible';
+        elem.style.opacity = '1';
+      }, index * 20);
+    });
   }
 }
 
